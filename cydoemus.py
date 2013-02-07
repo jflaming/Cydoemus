32b50f805482d1853160f0adc0596af9ea6532e3
R. Jay Flaming <jflaming@uw.edu>
2013-02-06 20:09:18 -0500

    fixed typos, added base module

---
+++ cydoemus.py
@@ -0,0 +1,17 @@
+#This is the primary importable module of the Cydoemus project. 
+#Initiated January, 2013
+#Creative Commons licensed, no commercial, attribution required
+#
+#
+
+#import arcpy
+
+#function for importing datasets
+
+
+#defining class for fuzzy sites
+
+
+#function for outputting presentation datasets and graphics
+
+print "hello world"
